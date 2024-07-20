0474ab251f9fccfffd3e83ccd45c4e1c9b37ac54
ManzXD <aimanmk89@gmail.com>
2024-07-20 19:58:10 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -312,351 +312,128 @@ const musicData = [
  * add eventListnere on all elements that are passed
  */
 
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
-  }
+loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
 }
 
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
 
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+    let Color1 = populate('#');
+    let Color2 = populate('#');
+    var angle = 'to right';
 
-/**
- * PLAYLIST
- * 
- * add all music in playlist, from 'musicData'
- */
-
-const playlist = document.querySelector("[data-music-list]");
-
-for (let i = 0, len = musicData.length; i < len; i++) {
-  playlist.innerHTML += `
-  <li>
-    <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
-      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
-        class="img-cover">
-
-      <div class="item-icon">
-        <span class="material-symbols-rounded">equalizer</span>
-      </div>
-    </button>
-  </li>
-  `;
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient;
 }
-
-
-
-/**
- * PLAYLIST MODAL SIDEBAR TOGGLE
- * 
- * show 'playlist' modal sidebar when click on playlist button in top app bar
- * and hide when click on overlay or any playlist-item
- */
-
-const playlistSideModal = document.querySelector("[data-playlist]");
-const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-
-const togglePlaylist = function () {
-  playlistSideModal.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("modalActive");
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
 }
-
-addEventOnElements(playlistTogglers, "click", togglePlaylist);
-
-
-
-/**
- * PLAYLIST ITEM
- * 
- * remove active state from last time played music
- * and add active state in clicked music
- */
-
-const playlistItems = document.querySelectorAll("[data-playlist-item]");
-
-let currentMusic = 0;
-let lastPlayedMusic = 0;
-
-const changePlaylistItem = function () {
-  playlistItems[lastPlayedMusic].classList.remove("playing");
-  playlistItems[currentMusic].classList.add("playing");
+function randomTrack(){
+    isRandom ? pauseRandom() : playRandom();
 }
-
-addEventOnElements(playlistItems, "click", function () {
-  lastPlayedMusic = currentMusic;
-  currentMusic = Number(this.dataset.playlistItem);
-  changePlaylistItem();
-});
-
-
-
-/**
- * PLAYER
- * 
- * change all visual information on player, based on current music
- */
-
-const playerBanner = document.querySelector("[data-player-banner]");
-const playerTitle = document.querySelector("[data-title]");
-const playerAlbum = document.querySelector("[data-album]");
-const playerYear = document.querySelector("[data-year]");
-const playerArtist = document.querySelector("[data-artist]");
-
-const audioSource = new Audio(musicData[currentMusic].musicPath);
-
-const changePlayerInfo = function () {
-  playerBanner.src = musicData[currentMusic].posterUrl;
-  playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
-  document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
-  playerTitle.textContent = musicData[currentMusic].title;
-  playerAlbum.textContent = musicData[currentMusic].album;
-  playerYear.textContent = musicData[currentMusic].year;
-  playerArtist.textContent = musicData[currentMusic].artist;
-
-  audioSource.src = musicData[currentMusic].musicPath;
-
-  audioSource.addEventListener("loadeddata", updateDuration);
-  playMusic();
+function playRandom(){
+    isRandom = true;
+    randomIcon.classList.add('randomActive');
 }
-
-addEventOnElements(playlistItems, "click", changePlayerInfo);
-
-/** update player duration */
-const playerDuration = document.querySelector("[data-duration]");
-const playerSeekRange = document.querySelector("[data-seek]");
-
-/** pass seconds and get timcode formate */
-const getTimecode = function (duration) {
-  const minutes = Math.floor(duration / 60);
-  const seconds = Math.ceil(duration - (minutes * 60));
-  const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  return timecode;
+function pauseRandom(){
+    isRandom = false;
+    randomIcon.classList.remove('randomActive');
 }
-
-const updateDuration = function () {
-  playerSeekRange.max = Math.ceil(audioSource.duration);
-  playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
+function repeatTrack(){
+    let current_index = track_index;
+    loadTrack(current_index);
+    playTrack();
 }
-
-audioSource.addEventListener("loadeddata", updateDuration);
-
-
-
-/**
- * PLAY MUSIC
- * 
- * play and pause music when click on play button
- */
-
-const playBtn = document.querySelector("[data-play-btn]");
-
-let playInterval;
-
-const playMusic = function () {
-  if (audioSource.paused) {
-    audioSource.play();
-    playBtn.classList.add("active");
-    playInterval = setInterval(updateRunningTime, 500);
-  } else {
-    audioSource.pause();
-    playBtn.classList.remove("active");
-    clearInterval(playInterval);
-  }
+function playpauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
 }
-
-playBtn.addEventListener("click", playMusic);
-
-
-/** update running time while playing music */
-
-const playerRunningTime = document.querySelector("[data-running-time");
-
-const updateRunningTime = function () {
-  playerSeekRange.value = audioSource.currentTime;
-  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-
-  updateRangeFill();
-  isMusicEnd();
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    track_art.classList.add('rotate');
+    wave.classList.add('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-
-
-
-/**
- * RANGE FILL WIDTH
- * 
- * change 'rangeFill' width, while changing range value
- */
-
-const ranges = document.querySelectorAll("[data-range]");
-const rangeFill = document.querySelector("[data-range-fill]");
-
-const updateRangeFill = function () {
-  let element = this || ranges[0];
-
-  const rangeValue = (element.value / element.max) * 100;
-  element.nextElementSibling.style.width = `${rangeValue}%`;
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    track_art.classList.remove('rotate');
+    wave.classList.remove('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
 }
-
-addEventOnElements(ranges, "input", updateRangeFill);
-
-
-
-/**
- * SEEK MUSIC
- * 
- * seek music while changing player seek range
- */
-
-const seek = function () {
-  audioSource.currentTime = playerSeekRange.value;
-  playerRunningTime.textContent = getTimecode(playerSeekRange.value);
+function nextTrack(){
+    if(track_index < music_list.length - 1 && isRandom === false){
+        track_index += 1;
+    }else if(track_index < music_list.length - 1 && isRandom === true){
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    }else{
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
 }
-
-playerSeekRange.addEventListener("input", seek);
-
-
-
-/**
- * END MUSIC
- */
-
-const isMusicEnd = function () {
-  if (audioSource.ended) {
-    playBtn.classList.remove("active");
-    audioSource.currentTime = 0;
-    playerSeekRange.value = audioSource.currentTime;
-    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-    updateRangeFill();
-  }
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }else{
+        track_index = music_list.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
 }
-
-
-
-/**
- * SKIP TO NEXT MUSIC
- */
-
-const playerSkipNextBtn = document.querySelector("[data-skip-next]");
-
-const skipNext = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
-  }
-
-  changePlayerInfo();
-  changePlaylistItem();
+function seekTo(){
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
 }
-
-playerSkipNextBtn.addEventListener("click", skipNext);
-
-
-
-/**
- * SKIP TO PREVIOUS MUSIC
- */
-
-const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
-
-const skipPrev = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
-  }
-
-  changePlayerInfo();
-  changePlaylistItem();
+function setVolume(){
+    curr_track.volume = volume_slider.value / 100;
 }
+function setUpdate(){
+    let seekPosition = 0;
+    if(!isNaN(curr_track.duration)){
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
 
-playerSkipPrevBtn.addEventListener("click", skipPrev);
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
+        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
-
-/**
- * SHUFFLE MUSIC
- */
-
-/** get random number for shuffle */
-const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
-
-const shuffleMusic = () => currentMusic = getRandomMusic();
-
-const playerShuffleBtn = document.querySelector("[data-shuffle]");
-let isShuffled = false;
-
-const shuffle = function () {
-  playerShuffleBtn.classList.toggle("active");
-
-  isShuffled = isShuffled ? false : true;
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
 }
-
-playerShuffleBtn.addEventListener("click", shuffle);
-
-
-
-/**
- * REPEAT MUSIC
- */
-
-const playerRepeatBtn = document.querySelector("[data-repeat]");
-
-const repeat = function () {
-  if (!audioSource.loop) {
-    audioSource.loop = true;
-    this.classList.add("active");
-  } else {
-    audioSource.loop = false;
-    this.classList.remove("active");
-  }
-}
-
-playerRepeatBtn.addEventListener("click", repeat);
-
-
-
-/**
- * MUSIC VOLUME
- * 
- * increase or decrease music volume when change the volume range
- */
-
-const playerVolumeRange = document.querySelector("[data-volume]");
-const playerVolumeBtn = document.querySelector("[data-volume-btn]");
-
-const changeVolume = function () {
-  audioSource.volume = playerVolumeRange.value;
-  audioSource.muted = false;
-
-  if (audioSource.volume <= 0.1) {
-    playerVolumeBtn.children[0].textContent = "volume_mute";
-  } else if (audioSource.volume <= 0.5) {
-    playerVolumeBtn.children[0].textContent = "volume_down";
-  } else {
-    playerVolumeBtn.children[0].textContent = "volume_up";
-  }
-}
-
-playerVolumeRange.addEventListener("input", changeVolume);
-
-
-/**
- * MUTE MUSIC
- */
-
-const muteVolume = function () {
-  if (!audioSource.muted) {
-    audioSource.muted = true;
-    playerVolumeBtn.children[0].textContent = "volume_off";
-  } else {
-    changeVolume();
-  }
-}
-
-playerVolumeBtn.addEventListener("click", muteVolume);
